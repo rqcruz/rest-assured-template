2a4b94d1575f0df2279e7b699fbc5454b24d4bfb
rqcruz <ronaldocruz183@gmail.com>
2023-11-02 17:04:24 -0300

    test:Created test to validate User requests

---
+++ src/test/java/tests/examples/user/UsersTest.java
@@ -0,0 +1,47 @@
+package tests.examples.user;
+
+import static org.hamcrest.Matchers.*;
+import org.junit.jupiter.api.Test;
+import requests.example.user.UserRequest;
+
+public class UsersTest {
+
+    private UserRequest userRequest;
+
+    @Test
+    public void requestShouldReturnClientData() {
+        userRequest = new UserRequest();
+
+        userRequest.requestByUserId("1")
+                .then()
+                .statusCode(200)
+                .body("id", is(1))
+                .body("email", is("john@gmail.com"))
+                .body("name.firstname", is("john"))
+                .body("name.lastname", is("doe"))
+        ;
+    }
+
+    @Test
+    public void requestLimitingTheQuantityOfUsers() {
+        userRequest = new UserRequest();
+
+        userRequest.requestLimitingQuantityOfUsers(2)
+                .then()
+                .statusCode(200)
+                .body("id", hasSize(2))
+        ;
+    }
+
+    @Test
+    public void requestUsingWordsAsIdShouldReturnAnError() {
+        userRequest = new UserRequest();
+
+        userRequest.requestByUserId("aaa")
+                .then()
+                .statusCode(400)
+                .body("status", is("error"))
+                .body("message", is("user id should be provided"))
+        ;
+    }
+}
