4845be4405c39c8e9791781c8df3eda76f6e621c
rqcruz <ronaldocruz183@gmail.com>
2023-11-05 13:56:52 -0300

    test:Adding test to update Product using patch request

--- src/test/java/tests/examples/product/ProductTest.java
+++ src/test/java/tests/examples/product/ProductTest.java
@@ -1,22 +1,42 @@
 package tests.examples.product;
 
 import org.junit.jupiter.api.Test;
-import requests.example.login.LoginRequest;
 import requests.example.product.ProductRequest;
 
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
 
 public class ProductTest {
 
     private ProductRequest productRequest;
 
     @Test
-    public void updatingProductWithSuccess() {
+    public void updatingProductWithSuccessUsingPutRequest() {
         productRequest = new ProductRequest();
 
-        productRequest.requestToUpdateProduct(
+        productRequest.requestToUpdateProductUsingPutRequest(
                 "7",
+                        "test product",
+                        13.5,
+                        "'lorem ipsum set",
+                        "'https://i.pravatar.cc",
+                        "electronic")
+                .then()
+                .statusCode(200)
+                .body("id", is(7))
+                .body("title", is("test product"))
+                .body("price", is(13.5F))
+                .body("description", is("'lorem ipsum set"))
+                .body("image", is("'https://i.pravatar.cc"))
+                .body("category", is("electronic"))
+        ;
+    }
+
+    @Test
+    public void updatingProductWithSuccessUsingPatchRequest() {
+        productRequest = new ProductRequest();
+
+        productRequest.requestToUpdateProductUsingPatchRequest(
+                        "7",
                         "test product",
                         13.5,
                         "'lorem ipsum set",
