612d5b81239d702b432d44b10433e95a11d9de86
rqcruz <ronaldocruz183@gmail.com>
2023-11-05 14:16:05 -0300

    test:Adding product delete test

--- src/test/java/tests/examples/product/ProductTest.java
+++ src/test/java/tests/examples/product/ProductTest.java
@@ -52,4 +52,22 @@ public class ProductTest {
                 .body("category", is("electronic"))
         ;
     }
+
+    @Test
+    public void deleteProductWithSuccess() {
+        productRequest = new ProductRequest();
+
+        productRequest.requestToDeleteProduct("6")
+                .then()
+                .statusCode(200)
+                .body("id", is(6))
+                .body("title", is("Solid Gold Petite Micropave "))
+                .body("price", is(168))
+                .body("category", is("jewelery"))
+                .body("description", is("Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days."))
+                .body("image", is("https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg"))
+                .body("rating.rate", is(3.9F))
+                .body("rating.count", is(70))
+        ;
+    }
 }
