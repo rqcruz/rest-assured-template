be69883f4ee8aeb919a30460ea8433abfa5ac746
rqcruz <ronaldocruz183@gmail.com>
2023-11-05 13:57:45 -0300

    feat:Adding patch request method

--- src/test/java/requests/example/product/ProductRequest.java
+++ src/test/java/requests/example/product/ProductRequest.java
@@ -9,7 +9,7 @@ public class ProductRequest {
 
     RestAssuredBase restAssuredBased;
 
-    public Response requestToUpdateProduct(String productId, String title, Double price, String description, String image, String category) {
+    public Response requestToUpdateProductUsingPutRequest(String productId, String title, Double price, String description, String image, String category) {
         restAssuredBased = new RestAssuredBase();
 
         return restAssuredBased
@@ -17,5 +17,15 @@ public class ProductRequest {
                 .body(new ProductPayload(title, price, description, image, category))
                 .setPathParam("id", productId)
                 .sendPutRequest(Endpoints.getUpdateProductPath());
+    }
+
+    public Response requestToUpdateProductUsingPatchRequest(String productId, String title, Double price, String description, String image, String category) {
+        restAssuredBased = new RestAssuredBase();
+
+        return restAssuredBased
+                .setDefaultRequestSpecification()
+                .body(new ProductPayload(title, price, description, image, category))
+                .setPathParam("id", productId)
+                .sendPatchRequest(Endpoints.getUpdateProductPath());
     }
 }
