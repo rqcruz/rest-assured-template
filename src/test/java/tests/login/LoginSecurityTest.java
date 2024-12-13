643dfbc1e526ff0e02f3f7505d57f358ae2ad5ad
rqcruz <ronaldocruz183@gmail.com>
2024-12-12 23:53:49 -0300

    refactor:Replacing multiple tests with just one using Dataprovider

--- src/test/java/tests/login/LoginSecurityTest.java
+++ src/test/java/tests/login/LoginSecurityTest.java
@@ -1,60 +1,31 @@
 package tests.login;
 
 import dto.LoginDTO;
-import factory.LoginFactory;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
 import client.login.LoginClient;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import tests.BaseApi;
 
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
 
 public class LoginSecurityTest extends BaseApi {
 
     LoginClient loginClient = new LoginClient();
 
-    @Test
     @Tag("regression")
     @Tag("negative-scenario")
-    @DisplayName("The request using bad credentials should return status code 401")
-    public void postToGetATokenWithBadCredentials() {
-        LoginDTO loginDTO = LoginFactory.setCredentials("fake", "fake");
-
+    @ParameterizedTest
+    @MethodSource("dataprovider.LoginProvider#provideInvalidCredentials")
+    @DisplayName("The request using invalid credentials should return status code 401")
+    public void postToGetATokenWithBadCredentials(LoginDTO loginDTO, String errorMsg) {
         loginClient
                 .getLoginToken(loginDTO)
                 .assertThat()
                 .statusCode(HttpStatus.SC_UNAUTHORIZED)
-                .body("html.body", is("username or password is incorrect"))
-        ;
-    }
-
-    @Test
-    @Tag("regression")
-    @Tag("negative-scenario")
-    @DisplayName("The request using invalid username should return status code 401")
-    public void postToGetATokenWithInvalidUsername() {
-        LoginDTO loginDTO = LoginFactory.setCredentials("fake", "83r5^_");
-
-        loginClient.getLoginToken(loginDTO)
-                .assertThat()
-                .statusCode(HttpStatus.SC_UNAUTHORIZED)
-                .body("html.body", is("username or password is incorrect"))
-        ;
-    }
-
-    @Test
-    @Tag("regression")
-    @Tag("negative-scenario")
-    @DisplayName("The request using invalid password should return status code 401")
-    public void postToGetATokenWithInvalidPassword() {
-        LoginDTO loginDTO = LoginFactory.setCredentials("mor_2314", "fake");
-
-        loginClient.getLoginToken(loginDTO)
-                .assertThat()
-                .statusCode(HttpStatus.SC_UNAUTHORIZED)
-                .body("html.body", is("username or password is incorrect"))
+                .body("html.body", is(errorMsg))
         ;
     }
 
