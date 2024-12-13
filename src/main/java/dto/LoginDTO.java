96811beaccbb070ea38af6413771aea2e2e28a00
rqcruz <ronaldocruz183@gmail.com>
2024-12-12 23:52:53 -0300

    feat:Using @ToString to display used values in the console

--- src/main/java/dto/LoginDTO.java
+++ src/main/java/dto/LoginDTO.java
@@ -1,14 +1,12 @@
 package dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Builder
 @Getter
 @Setter
+@ToString
 @AllArgsConstructor
 @JsonIgnoreProperties
 public class LoginDTO {
