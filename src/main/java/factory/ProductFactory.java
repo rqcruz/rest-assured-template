140ce13c74e9ea31e643adca5d6ee5d65a0ac810
rqcruz <ronaldocruz183@gmail.com>
2025-01-28 21:49:25 -0300

    feat:Data generated with Datafaker

--- src/main/java/factory/ProductFactory.java
+++ src/main/java/factory/ProductFactory.java
@@ -1,16 +1,25 @@
 package factory;
 
 import dto.ProductDTO;
+import net.datafaker.Faker;
 
 public class ProductFactory {
 
+    private static Faker faker = new Faker();
+    private static final String TITLE = faker.commerce().productName();
+    private static final double PRICE = Double.parseDouble(faker.commerce().price());
+    private static final String SENTENCE = faker.lorem().sentence(10);
+    private static final String URL = faker.internet().url();
+    private static final String CATEGORY = faker.commerce().department();
+
+
     public static ProductDTO getValidProductValues() {
         return ProductDTO.builder()
-                .title("Brazilian Macaw Smartwatch")
-                .price(14.0)
-                .description("A smartwatch designed in honor of the Brazilian macaw")
-                .image("https://fakestoreapi.com/img/br_macaw_IMG123.jpg")
-                .category("gadget")
+                .title(TITLE)
+                .price(PRICE)
+                .description(SENTENCE)
+                .image(URL)
+                .category(CATEGORY)
                 .build();
     }
 }
